d685292a5104bd5bdd3b1c1fbce58e19de6e4b40
Antony <antony.correoso@laposte.net>
2018-08-23 12:41:40 +0200

    End of TP2.1

--- src/main/java/demo_tests_unitaires/demo_tests_unitaires/App.java
+++ src/main/java/demo_tests_unitaires/demo_tests_unitaires/App.java
@@ -1,13 +1,17 @@
 package demo_tests_unitaires.demo_tests_unitaires;
 
+import fr.dev.util.StringUtils;
+import fr.test.util.StringUtilsTest;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println(StringUtils.levenshteinDistance("", "instance"));
+		new StringUtilsTest();
     }
 }
---
+++ src/test/java/ExecuteTest.java
@@ -0,0 +1,9 @@
+import fr.dev.util.StringUtils;
+import fr.test.util.StringUtilsTest;
+
+public class ExecuteTest {
+    public static void main( String[] args ) throws Exception
+    {
+		new StringUtilsTest();
+    }
+}
